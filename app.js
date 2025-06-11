c181158afbcc78b43d12ae8f34379deb9a84278e
yuzumikan3-kan <2201115059xk@cyber-u.ac.jp>
2025-06-12 00:37:33 +0900

    add Node api

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var YesNoRouter = require('./routes/YesNo');
+var NodeRouter = require('./routes/Node');
 
 var app = express();
 
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat',catRouter);
-app.use('/YesNo',YesNoRouter);
+app.use('/Node',NodeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
